b698d0e8a2b948182494421ae51a8a7388f2da14
duksdemo <duks1802@gmail.com>
2024-08-06 14:34:51 +0530

    First commit - branch to add A and B

---
+++ src/main/java/com/git_example/test/A.java
@@ -0,0 +1,7 @@
+package com.git_example.test;
+
+public class A {
+    public static void main(String[] args){
+        System.out.println("Hello, Git!");
+    }
+}
---
+++ src/main/java/com/git_example/test/B.java
@@ -0,0 +1,7 @@
+package com.git_example.test;
+
+public class B {
+    public void doSomethingB() {
+        System.out.println("Doing something in B");
+    }
+}
