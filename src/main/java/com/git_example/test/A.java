60ec4d3a3f96a895ccbcc449d9455ca43cc7787e
duksdemo <duks1802@gmail.com>
2024-08-06 14:46:25 +0530

    Second commit - branch to add A and B

--- src/main/java/com/git_example/test/A.java
+++ src/main/java/com/git_example/test/A.java
@@ -3,5 +3,6 @@ package com.git_example.test;
 public class A {
     public static void main(String[] args){
         System.out.println("Hello, Git World!");
+        System.out.println("This is a new feature.");
     }
 }
