67c6be062266d88f789475160040122d36c31b4d
duksdemo <duks1802@gmail.com>
2024-08-06 14:42:16 +0530

    Second commit

--- src/main/java/com/git_example/test/A.java
+++ src/main/java/com/git_example/test/A.java
@@ -2,6 +2,6 @@ package com.git_example.test;
 
 public class A {
     public static void main(String[] args){
-        System.out.println("Hello, Git!");
+        System.out.println("Hello, Git World!");
     }
 }
